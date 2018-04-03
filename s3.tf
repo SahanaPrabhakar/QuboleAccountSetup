0d2fb67039fbd11865e27b8eb487391d5ca8d4a5
Sahana Prabhakar <sahanasp80@gmail.com>
2018-04-03 14:08:42 +0100

    Added defloc file as recommended by Qubole

--- s3.tf
+++ s3.tf
@@ -7,3 +7,10 @@ resource "aws_s3_bucket" "sparkstoragedemo" {
     Environment = "Qubole"
   }
 }
+
+resource "aws_s3_bucket_object" "folder1" {
+    bucket = "${aws_s3_bucket.sparkstoragedemo.id}"
+    acl    = "private"
+    key    = "defloc/"
+    source = "emptyfile.txt"
+}
